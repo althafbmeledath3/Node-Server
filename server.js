5d801d44cb00ccef365b8cac6f77b08f3f269de0
Althaf B Meledath <althafbmeledath@Althafs-MacBook-Air.local>
2025-03-27 16:52:32 +0530

    third

--- server.js
+++ server.js
@@ -1,79 +1,93 @@
+const http = require("http");
 
-const http = require('http')
+const fs = require("fs");
 
-const fs = require('fs')
+const path = require("path");
 
-const path = require('path')
+const queryString = require("querystring");
 
-const htmlPath = path.join(__dirname,'public',"index.html")
-const cssPath = path.join(__dirname,"public","style.css")
-const scriptPath = path.join(__dirname,"public","script.js")
+const { json } = require("stream/consumers");
+
+const htmlPath = path.join(__dirname, "public", "index.html");
+const cssPath = path.join(__dirname, "public", "style.css");
+const scriptPath = path.join(__dirname, "public", "script.js");
 
 console.log(htmlPath, cssPath);
 
+server = http.createServer((req, res) => {
+  console.log(req.method);
+  if (req.method === "GET") {
+    console.log("Current Request");
+    console.log(req.url);
 
+    if (req.url === "/") {
+      fs.readFile(htmlPath, (error, data) => {
+        if (error) {
+          res.writeHead(500);
+          res.end(error.message);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        }
+      });
+    }
+    if (req.url === "/style.css") {
+      fs.readFile(cssPath, (error, data) => {
+        if (error) {
+          res.writeHead(500);
+          res.end(error.message);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/css" });
+          res.end(data);
+        }
+      });
+    }
 
-server = http.createServer((req,res)=>{
+    if (req.url === "/script.js") {
+      fs.readFile(scriptPath, (error, data) => {
+        if (error) {
+          res.writeHead(500);
+          res.end(error.message);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/script" });
+          res.end(data);
+        }
+      });
+    }
+  } 
+  else if (req.method == "POST") {
+    if (req.url === "/send-data") {
+      let body = "";
 
-    if(req.method==="GET"){
-        console.log("Current Request");
-        
-        
-        if(req.url==="/"){
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
 
-            fs.readFile(htmlPath,(error,data)=>{
-
+      req.on("end", () => {
+        let parsed = queryString.parse(body);
+        fs.readFile("./data.json",(error,data)=>{
+            
+            let contents = error ? [] : JSON.parse(data)
+            contents.push(parsed)
+            fs.writeFile("./data.json",JSON.stringify(contents),error=>{
                 if(error){
                     res.writeHead(500)
                     res.end(error.message)
+                }
+                else{
+                    res.writeHead(200)
+                    res.end("Successfully recieved Data")
                     
                 }
-                else{
-
-                    res.writeHead(200,{"Content-Type":"text/html"})
-                    res.end(data)
-                }
-            })  
-        }   
-        if(req.url==="/style.css"){
-            fs.readFile(cssPath,(error,data)=>{
-                if(error){
-                    res.writeHead(500)
-                    res.end(error.message)
-                }
-                else{
-                    res.writeHead(200,{"Content-Type":"text/css"})
-                    res.end(data)
-                }
             })
-        }
-
-        if(req.url=="/script.js"){
-            fs.readFile(scriptPath,(error,data)=>{
-                if(error){
-                    res.writeHead(500)
-                    res.end(error.message)
-                }
-                else{
-                    res.writeHead(200,{"Content-Type":"text/script"})
-                    res.end(data)
-                }
-            })
-        }
-
-        console.log(req.url);
-        
+        })
+      });
     }
-})
+  }
+});
 
-let port = 3000
+let port = 3000;
 
-server.listen(port,()=>{
-    console.log("Server Running",port);
-    
-})
-
-
-
-
-
+server.listen(port, () => {
+  console.log("Server Running", port);
+});
